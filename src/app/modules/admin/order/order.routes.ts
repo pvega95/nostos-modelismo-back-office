128ce6beb8547b050599f789e5a4d08fe1afe225
Paul Vega <paulmax951@gmail.com>
2024-08-25 01:16:30 -0500

    feat(ECNO-7): updating sales component, services

--- src/app/modules/admin/order/order.routes.ts
+++ src/app/modules/admin/order/order.routes.ts
@@ -1,10 +1,15 @@
 import { OrderComponent } from 'app/modules/admin/order/order.component';
 
 import { Routes } from '@angular/router';
+import { OrderService } from './order.service';
+import { inject } from '@angular/core';
 
 export default [
     {
         path: '',
-        component: OrderComponent
+        component: OrderComponent,
+        resolve: {
+            brands: () => inject(OrderService).getOrders(),
+        }
     },
 ] as Routes;
---
+++ src/app/modules/admin/sales/sales.types.ts
@@ -0,0 +1,3 @@
+export interface Sales {
+    [T: string]: any
+}
